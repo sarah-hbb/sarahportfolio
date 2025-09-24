3a942349e1a0b20f5e6281c6123f8b7e79aa786f
sarah-hbb <sarahabibi.89@gmail.com>
2025-09-24 17:42:21 +0200

    install cooki parse

--- api/index.ts
+++ api/index.ts
@@ -46,7 +46,7 @@ app.use("/api/comment", commentRoutes);
 //app.use("/api/summary", summaryRoutes);
 
 app.use(express.static(path.join(__dirname, "../client/build")));
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
 });
 
